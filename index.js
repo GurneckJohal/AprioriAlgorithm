4dece89f364eecd0d597ff0cb0966e5352b6cf9b
GurneckJohal <gurneckjohal@gmail.com>
2022-08-28 15:55:51 -0600

    removed database code
    removed code that interacted with database

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
-
-const express = require("express");
-const cors = require("cors");
-
-const app = express();
-app.use(express.json());
-app.use(cors({ origin: true }));
-
-admin.initializeApp();
-
 const fpmConstants = {
   MIN_SUPPORT: 0.5,
   MIN_CONF: 0.5,
@@ -373,50 +361,7 @@ function generateRules(combos, input) {
   return rules;
 }
 
-function AuthorizationMiddleware(req, res, next) {
-  const token = Buffer.from(req.headers.authorization, "base64").toString();
-
-  admin
-    .auth()
-    .verifyIdToken(token)
-    .then((user) => {
-      res.locals.currentUser = user;
-
-      next();
-
-      return;
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-}
-
-// Add 'recommendation' after '/' when testing the express server hosted locally
-app.get("/recommendation", AuthorizationMiddleware, async (req, res) => {
-  // UNDO
-  const currentUser = res.locals.currentUser;
-
-  let dbUser = await admin
-    .database()
-    .ref(`/users/${currentUser.uid}`) // UNDO
-    .once("value");
-  dbUser = dbUser.val();
-
-  const result = {
-    ...dbUser,
-  };
-  //console.log(`1: ${currentUser.uid}`);
-  let visitedSites = await admin.database().ref(`/visitedSites`).once("value");
-  if (visitedSites != null) {
-    //console.log(visitedSites.val());
-    visitedSites = Object.entries(visitedSites.val()).filter(
-      ([key, value]) => key != currentUser.uid
-    );
-    //console.log(visitedSites);
-    visitedSites = Object.values(Object.fromEntries(visitedSites)).map((x) =>
-      Object.entries(x).map((y) => y[0])
-    );
-    //console.log(visitedSites);
+function testApriori{
     const initInput = [
       ["1", "2", "4", "5", "6"],
       ["1", "3", "4", "5", "7"],
@@ -435,26 +380,6 @@ app.get("/recommendation", AuthorizationMiddleware, async (req, res) => {
       ["Bob", "Mike"],
     ];
     FPM(initInput);
-  } else {
-    visitedSites = [];
   }
-
-  // TODO: Replace with user type enumerations
-  switch (dbUser.type) {
-    case 1:
-      result.message = "Generate recommendation for traveller";
-      break;
-    case 2:
-      result.message = "Generate recommendation for site owner";
-      break;
-  }
-  console.log("Hello");
-  res.send(result);
 });
 
-// Used to test the express server hosted locally
-app.listen(8000, () => {
-  console.log("Server started at http://localhost:8000/");
-});
-
-exports.recommendation = functions.https.onRequest(app);
